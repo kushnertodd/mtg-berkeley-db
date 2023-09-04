6308fbbf002423b206e1bc57c159cf0899808177
Todd Kushner <kushnertodd@gmail.com>
2023-09-04 11:36:08 -0600

    final version for v0.2, check src/docker/instructions.txt and HISTORY.md for instructions

--- src/scripts/check.sh
+++ src/scripts/check.sh
@@ -1,7 +1,7 @@
 sh rm-db-samples.sh
 for f in `cat ../json/request-names.txt` ; do
   out=$f.txt
-  sh request.sh $f
+  sh request.sh "$f"
   if diff results/$out test/$out >/dev/null ; then
     echo "succeed $f"
   else
